16a6d474cc86b951425ec3bfce1b79cbf18111f4
unho.chang <capcock@snu.ac.kr>
2023-12-19 16:11:26 +0900

    템플릿을 이용한 메인페이지 HTML 생성

--- lists/tests.py
+++ lists/tests.py
@@ -5,7 +5,4 @@ from django.http import HttpRequest
 class HomePageTest(TestCase):
     def test_home_page_returns_correct_html(self):
         response = self.client.get("/")
-        self.assertContains(response, "<title>To-Do lists</title>")
-        self.assertContains(response, "<html>")
-        self.assertContains(response, "</html>")
-        self.assertTemplateUsed(response, "wrong.html")
+        self.assertTemplateUsed(response, "home.html")
