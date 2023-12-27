5cc07c3fec26d769fdced2199583d186f320fd75
unho.chang <capcock@snu.ac.kr>
2023-12-27 13:08:34 +0900

    POST후에 리디렉션과 템플릿에 모든 작업 아이템 표시

--- lists/views.py
+++ lists/views.py
@@ -5,11 +5,10 @@ from lists.models import Item
 # Create your views here.
 def home_page(request):
     if request.method == "POST":
-        item = Item()
-        item.text = request.POST["item_text"]
-        item.save()
+        Item.objects.create(text = request.POST["item_text"])
         return redirect("/")
-
+    
+    items = Item.objects.all()
     return render(request, "home.html", {
-        "new_item_text": request.POST.get("item_text","")},
+        "items": items},
     )
