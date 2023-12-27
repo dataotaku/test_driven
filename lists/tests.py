12727ccbfcf45333908f363bec42dfa0df51ae78
unho.chang <capcock@snu.ac.kr>
2023-12-27 12:20:30 +0900

    책홈피와는 달리 에러가 남. redirect 함수 검사 필요예상

--- lists/tests.py
+++ lists/tests.py
@@ -10,8 +10,15 @@ class HomePageTest(TestCase):
 
     def test_can_save_a_POST_request(self):
         response = self.client.post("/", data={"item_text": "A new list item"})
-        self.assertContains(response, "A new list item")
-        self.assertTemplateUsed(response, "home.html")
+        self.assertEqual(Item.objects.count(), 1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, "A new list item")
+
+        self.assertRedirects(response, "A new list Item")
+
+    def test_only_saves_items_when_necessary(self):
+        self.client.get("/")
+        self.assertEqual(Item.objects.count(), 0)
 
 class ItemModelTest(TestCase):
     def test_saving_and_retrieving_items(self):
--- lists/views.py
+++ lists/views.py
@@ -1,8 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.http import HttpResponse
+from lists.models import Item
 
 # Create your views here.
 def home_page(request):
+    if request.method == "POST":
+        item = Item()
+        item.text = request.POST["item_text"]
+        item.save()
+        return redirect("/")
+
     return render(request, "home.html", {
         "new_item_text": request.POST.get("item_text","")},
     )
